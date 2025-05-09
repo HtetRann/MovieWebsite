614420e1b4480df763a6a9c6b91f2b1fa89e20fb
*4NGhxgKSPV <htetrann@gmail.com>
2025-05-10 01:29:46 +0900

    commit

--- src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Header.css";
-
-const Header = () => {
-  return <span onClick={() => window.scroll(0,0)} className="header">🎃Plot Entertainment🎃</span>
-
-};
-
-export default Header
